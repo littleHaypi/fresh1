432197e01f50ad648f93918b71a5753ce140c31a
littleHaypi <2911337127@qq.com>
2020-10-06 17:00:47 +0800

    初始提交

---
+++ fresh-zuul/src/main/java/com/nll/fresh_zuul/App.java
@@ -0,0 +1,13 @@
+package com.nll.fresh_zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
